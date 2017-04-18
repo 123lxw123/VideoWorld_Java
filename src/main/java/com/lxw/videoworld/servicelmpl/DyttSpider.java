d39cd7d5bfe1eaad6cb9a6ecf01efba0218ddf55
lxw <342466322@qq.com>
2017-04-18 23:29:15 +0800

    提取电影天堂首页 url 及 title

--- src/main/java/com/lxw/videoworld/servicelmpl/DyttSpider.java
+++ src/main/java/com/lxw/videoworld/servicelmpl/DyttSpider.java
@@ -1,7 +1,7 @@
 package com.lxw.videoworld.servicelmpl;
 
-import com.lxw.videoworld.spider.BaseProcessor;
 import com.lxw.videoworld.spider.DyttHomePagePipeline;
+import com.lxw.videoworld.spider.DyttHomePageProcessor;
 import com.lxw.videoworld.utils.URLUtil;
 import us.codecraft.webmagic.Spider;
 
@@ -11,7 +11,7 @@ import us.codecraft.webmagic.Spider;
 public class DyttSpider {
 
     public static void main(String args[]) {
-        Spider.create(new BaseProcessor()).thread(10)
+        Spider.create(new DyttHomePageProcessor()).thread(1)
          .addUrl(URLUtil.URL_DYTT_HOME_PAGE)
          .addPipeline(new DyttHomePagePipeline())
          .run();
---
+++ src/main/java/com/lxw/videoworld/spider/DyttHomePageProcessor.java
@@ -0,0 +1,19 @@
+package com.lxw.videoworld.spider;
+
+import us.codecraft.webmagic.Page;
+
+import java.util.List;
+
+/**
+ * Created by Zion on 2017/4/18.
+ */
+public class DyttHomePageProcessor extends BaseProcessor{
+    @Override
+    public void process(Page page) {
+        super.process(page);
+        List<String> urlList = page.getHtml().css("div.co_content2").links().all();
+        List<String> titleList = page.getHtml().css("div.co_content2").css("a").regex(">(.*?)</a>").all();
+        urlList.addAll(page.getHtml().css("div.co_content4").links().all());
+        titleList.addAll(page.getHtml().css("div.co_content4").css("a").regex(">(.*?)</a>").all());
+    }
+}
